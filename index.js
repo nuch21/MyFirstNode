b3a0830c635d7e2680144ba9f600fe64cfb9658b
nuch21 <kewali_tn@hotmail.com>
2017-09-23 12:07:27 +0700

    commit

--- index.js
+++ index.js
@@ -34,13 +34,17 @@ app.listen(3000, function() {
 
 
 // add to file ./index.js
+import mongoose from './config/mongoose';
 import express from './config/express';
+
 
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.PORT = process.env.PORT || 3000;
 
+var db = mongoose();
 var app = express();
+
 
 app.listen(process.env.PORT, () => {
        console.log('Starting node.js on port ' + process.env.PORT);
