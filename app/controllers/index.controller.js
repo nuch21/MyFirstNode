09a4509451e52a78cb882c9ba7b2727c75757448
nuch21 <kewali_tn@hotmail.com>
2017-09-23 10:44:53 +0700

    commit

---
+++ app/controllers/index.controller.js
@@ -0,0 +1,7 @@
+exports.index = (req, res) => {
+    res.send('<b>My name is Grace, Im study in mahidol university</b>');
+}
+
+exports.help = (req, res) => {
+    res.send('<h1>I love Grace MU.</h1></br><img border="0" alt="" src="./kaudha1-703x422.jpg" width="100" height="100">');
+}
